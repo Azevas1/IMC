8e69fcc6061c87f685b208f15e73bda257f5e41d
Azevas1 <85646897+Azevas1@users.noreply.github.com>
2021-06-11 12:55:25 -0300

    Update main.js

--- main.js
+++ main.js
@@ -7,11 +7,6 @@ function calcular(){
   let altura = (alt.value)
   let resultado = peso / (altura*altura)
 
-
-  if(!peso || !altura ){
-    IMC.innerHTML = "Informação Inválida"
-}
-
   console.log(peso)
   console.log(altura)
   console.log(resultado.toFixed(2))
@@ -34,29 +29,8 @@ function calcular(){
 }else{
     IMC.innerHTML = "Informação Inválida"
 
-
 }
 
 }
 
 
-/*let altura = 1.89
-let peso = 150
-let IMC = peso / (altura * altura)
-
-if(peso / (altura * altura) < 18.5){
-    console.log("Seu Imc é", IMC.toFixed(2),",","Abaixo Do Peso")
-} else if(peso / (altura * altura) > 18.5 && (peso / (altura * altura) < 24.9)){
-    console.log("Seu Imc é", IMC.toFixed(2),",","Peso Normal")
-}else if(peso / (altura * altura) > 25 && (peso / (altura * altura) < 29.9)){
-    console.log("Seu Imc é", IMC.toFixed(2),",","Sobre Peso")
-}else if(peso / (altura * altura) > 30 && (peso / (altura * altura) < 34.9)){
-    console.log("Seu Imc é", IMC.toFixed(2),",","Obesidade Grau 1")
-}else if(peso / (altura * altura) > 35 && (peso / (altura * altura) < 39.9)){
-    console.log("Seu Imc é", IMC.toFixed(2),",","Obesidade Grau 2")
-}else if(peso / (altura * altura) > 40){
-    console.log("Seu Imc é", IMC.toFixed(2),",","Obesidade Grau 3")
-}else{
-    console.log("Informação Inválida")
-}
-*/
